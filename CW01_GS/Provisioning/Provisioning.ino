487e30a26999e8768c51603d0601de557434a8c8
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2020-01-07 16:23:31 +0200

    added provisioning test

--- CW01_GS/Provisioning/Provisioning.ino
+++ CW01_GS/Provisioning/Provisioning.ino
@@ -5,86 +5,89 @@
 xProvision prv;
 xOD01 OD01;
 
-String ssid, password;
+String ssid, password, unit_name;
+const char* host = "www.google.com";
+const uint16_t port = 80;
 
 void setup() {
   // Set RGB Pins as outputs
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
 
-    Wire.begin();
+  Wire.begin();
   OD01.begin();
 
-  // put your setup code here, to run once:
   prv.begin();
   prv.addWiFi();
+  prv.addVariable("Unit Name:", "Choose a name for your XK07")
 
   prv.transmit();
-  //Writes this string, with the CRLF and SYNC prefixed and CRLF suffixed
-  //$!${"WiFi_Network":"Your SSID","WiFi_Password":"Your PSK"}
-
   prv.receive();
-  //Reads this string, with CRLF suffixed
-  //{"WiFi_Network":"Your-SSID","WiFi_Password":"Your-PSK"}
+
 
   if (prv.success())
   {
     prv.getWiFi(ssid, password);
     WiFi.begin(ssid.c_str(), password.c_str());
 
+    prv.getVariable("Unit Name:", unit_name);
+
+    int count {1};
+    OD01.clear();
+    OD01.println("Connecting to WiFi...");
+    OD01.println("");
+    
+
     while (WiFi.status() != WL_CONNECTED) {
       digitalWrite(LED_RED, HIGH);
+      OD01.println(".  .  .  .");      
+      OD01.clear(32*(1+(count%4)),128,2,6);
       delay(250);
       digitalWrite(LED_RED, LOW);
       delay(250);
+      count++;
     }
-    OD01.println(ssid);
-    OD01.println("IP address: ");
-    OD01.println(WiFi.localIP());
-    OD01.println(password);
+
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, HIGH);
+    OD01.set1X();
+    OD01.clear()
+    OD01.println("Connected");
+    OD01.println("Checking Internet");
+    OD01.println("   Connection...")
+    
+    WiFiClient client;
+    if (!client.connect(host, port)) {
+      Serial.println("connection failed");
+      OD01.clear();
+      OD01.println("Could Not Connect ");
+      OD01.println("   to Internet");
+      OD01.println("Please check your");
+      OD01.println("Internet Connection");
+      OD01.println("  and try again")
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_GREEN, LOW);
+      delay(5000);
+      return;
+    }
+    else
+    {
+      Serial.println("Connected");
+    }
+    digitalWrite(LED_RED, LOW);
     digitalWrite(LED_GREEN, HIGH);
   }
   else
   {
     prv.fail();
+    OD01.println("Unable to get");
+    OD01.println("  WiFi Data");
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, LOW);
   }
 }
 
 void loop()
 {
-  prv.transmit();
-  //Writes this string, with the CRLF and SYNC prefixed and CRLF suffixed
-  //$!${"WiFi_Network":"Your SSID","WiFi_Password":"Your PSK"}
-
-  prv.receive();
-  //Reads this string, with CRLF suffixed
-  //{"WiFi_Network":"Your-SSID","WiFi_Password":"Your-PSK"}
-
-  if (prv.success())
-  {
-    prv.getWiFi(ssid, password);
-    WiFi.begin(ssid.c_str(), password.c_str());
-
-    while (WiFi.status() != WL_CONNECTED) {
-      digitalWrite(LED_RED, HIGH);
-      delay(250);
-      digitalWrite(LED_RED, LOW);
-      delay(250);
-    }
-    OD01.println(ssid);
-    OD01.println("IP address: ");
-    OD01.println(WiFi.localIP());
-    OD01.println(password);
-    digitalWrite(LED_GREEN, HIGH);
-    
-  }
-  else
-  {
-    prv.fail();
-    Serial.println("PRV FAIL");
-  }
-
-  delay(5000);
-
-  
+   
 }
