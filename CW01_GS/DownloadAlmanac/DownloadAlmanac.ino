1fd85bc10d912ce5dd6ba9c468b4d5032178f4bd
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2020-01-08 12:44:27 +0200

    added almanac downloading

---
+++ CW01_GS/DownloadAlmanac/DownloadAlmanac.ino
@@ -0,0 +1,73 @@
+#include <xOD01.h>
+#include <ESP8266WiFi.h>
+
+xOD01 OD01;
+
+String ssid, password;
+
+const char* host = "www.agps.u-blox.com";
+const uint16_t port = 46434;
+
+void setup()
+{
+    Wire.begin();
+    OD01.begin();
+    OD01.prinln("Getting GPS Data...");
+
+    ssid = "XinaBox";
+    password = "RapidIoT";
+
+    WiFiClient client;
+    if (!client.connect(host, port)) {
+        Serial.println("connection failed");
+        OD01.clear();
+        OD01.println("Could Not Connect ");
+        OD01.println("   to Internet");
+        OD01.println("Please check your");
+        OD01.println("Internet Connection");
+        OD01.println("  and try again");
+        digitalWrite(LED_RED, HIGH);
+        digitalWrite(LED_GREEN, LOW);
+        return;
+    }
+    else
+    {
+        Serial.println("Connected");
+        OD01.println("");
+        OD01.println("Connected to Internet");
+        OD01.println("");
+
+    }
+    OD01.clear();
+    OD01.println("sending request...");
+
+    if (client.connected()) {
+        client.println("https://online-live1.services.u-blox.com/GetOnlineData.ashx?token=XXXXXXXXXXXXXXXXXXXXXX;gnss=gps;datatype=eph,alm,aux;");
+    }
+
+
+    // wait for data to be available
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            delay(60000);
+            return;
+    }
+  }
+
+  while (client.available()) {
+    char ch = static_cast<char>(client.read());
+    Serial.print(ch);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+  client.stop();
+}
+
+void loop()
+{
+   
+}
--- CW01_GS/Provisioning/Provisioning.ino
+++ CW01_GS/Provisioning/Provisioning.ino
@@ -1,6 +1,7 @@
 #include <ArduinoJson.h>    // https://github.com/bblanchon/ArduinoJson
 #include <xProvision.h>     // https://github.com/xinabox/arduino-Provision    
 #include <xOD01.h>
+#include <ESP8266WiFi.h>
 
 xProvision prv;
 xOD01 OD01;
@@ -9,17 +10,20 @@ String ssid, password, unit_name;
 const char* host = "www.google.com";
 const uint16_t port = 80;
 
+const char* host_gps = "www.agps.u-blox.com";
+const uint16_t port_gps = 46434;
+
 void setup() {
   // Set RGB Pins as outputs
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
-
+  delay(5000);
   Wire.begin();
   OD01.begin();
 
   prv.begin();
   prv.addWiFi();
-  prv.addVariable("Unit Name:", "Choose a name for your XK07")
+  prv.addVariable("Unit Name:", "Choose a name for your XK07");
 
   prv.transmit();
   prv.receive();
@@ -34,27 +38,61 @@ void setup() {
 
     int count {1};
     OD01.clear();
-    OD01.println("Connecting to WiFi...");
+    OD01.set2X();
+    OD01.println("Connecting ");
+    OD01.println(" to WiFi...");
     OD01.println("");
+    OD01.set1X();
     
-
     while (WiFi.status() != WL_CONNECTED) {
       digitalWrite(LED_RED, HIGH);
-      OD01.println(".  .  .  .");      
-      OD01.clear(32*(1+(count%4)),128,2,6);
+      
+      OD01.println("   .   ");
       delay(250);
       digitalWrite(LED_RED, LOW);
       delay(250);
+      digitalWrite(LED_RED, HIGH);
+      OD01.clear(0,128,6,8);
+      OD01.println("   .   .   ");
+      delay(250);
+      digitalWrite(LED_RED, LOW);
+      delay(250);
+      digitalWrite(LED_RED, HIGH);
+      OD01.clear(0,128,6,8);
+      OD01.println("   .   .   .   ");
+      delay(250);
+      digitalWrite(LED_RED, LOW);
+      delay(250);
+      digitalWrite(LED_RED, HIGH);
+      OD01.clear(0,128,6,8);
+      OD01.println("   .   .   .   .");
+      delay(250);
+      digitalWrite(LED_RED, LOW);
+      delay(250);
+      OD01.clear(0,128,6,8);
+
+      if(count>30)     //times out after 1 minute
+      {
+        OD01.clear();
+        OD01.println("");
+        OD01.println("");
+        OD01.println("  Could not connect ");
+        OD01.println("       to WiFi");
+        OD01.println("");
+        OD01.println("   Please try again");
+        return;
+      }
       count++;
     }
 
     digitalWrite(LED_RED, LOW);
     digitalWrite(LED_GREEN, HIGH);
     OD01.set1X();
-    OD01.clear()
-    OD01.println("Connected");
-    OD01.println("Checking Internet");
-    OD01.println("   Connection...")
+    OD01.clear();
+    OD01.println("   WiFi Connected");
+    OD01.println("");
+    OD01.println("  Checking Internet");
+    OD01.println("    Connection...");
     
     WiFiClient client;
     if (!client.connect(host, port)) {
@@ -64,18 +102,21 @@ void setup() {
       OD01.println("   to Internet");
       OD01.println("Please check your");
       OD01.println("Internet Connection");
-      OD01.println("  and try again")
+      OD01.println("  and try again");
       digitalWrite(LED_RED, HIGH);
       digitalWrite(LED_GREEN, LOW);
-      delay(5000);
       return;
     }
     else
     {
       Serial.println("Connected");
+      OD01.println("");
+      OD01.println("Connected to Internet");
+      OD01.println("");
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_GREEN, HIGH);
     }
-    digitalWrite(LED_RED, LOW);
-    digitalWrite(LED_GREEN, HIGH);
+    
   }
   else
   {
@@ -84,7 +125,17 @@ void setup() {
     OD01.println("  WiFi Data");
     digitalWrite(LED_RED, HIGH);
     digitalWrite(LED_GREEN, LOW);
+    return;
   }
+  
+  delay(5000);
+  OD01.clear();
+  OD01.println("Unit Name:");
+  OD01.print("    ");
+  OD01.print(unit_name);
+
+  
+
 }
 
 void loop()
